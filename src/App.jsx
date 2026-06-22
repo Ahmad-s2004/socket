abc93c0425077d9dba5b3264d3651466b0a17f55
Ahmad-s2004 <shahidahmadamman@gmail.com>
2026-06-22 14:41:01 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Home from './pages/home'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Payment from './pages/payment'
 import DestinationPage from './pages/destination'
 import Contact from './pages/contact'
